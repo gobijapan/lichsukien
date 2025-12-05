70cef6b40ddfde921a9d505bb3481ea671aebfc7
gobijapan <gobijapan@gmail.com>
2025-12-05 12:20:11 +0900

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -2,6 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+// Register Service Worker for Firebase Cloud Messaging
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/firebase-messaging-sw.js')
+    .then((registration) => {
+      console.log('Service Worker registered successfully:', registration);
+    })
+    .catch((err) => {
+      console.log('Service Worker registration failed:', err);
+    });
+}
+
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
